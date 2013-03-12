32e2f61b5dd43e6b48eb3c77c55f2db39df9f0b5
Tobias Bosch <tobias.bosch@opitz-consulting.com>
2013-03-12 10:52:04 +0100

    added regression spec for #7.

--- test/ui/regressionSpec.js
+++ test/ui/regressionSpec.js
@@ -1,9 +1,10 @@
 describe('regression', function() {
-    var uit = uitest.current;
-    uit.url("../test/ui/fixtures/basic.html");
+    var uit = uitest.current,
+        relPath = "../test/ui/fixtures/basic.html";
 
-    describe('issue 8: Setting and returning hash', function() {
+    describe('issue 8 and 9: Setting and returning hash', function() {
         it('should allow to change the hash without a reload', function() {
+            uit.url(relPath);
             uit.runs(function(window) {
                 window.flag = true;
                 window.location.hash = 'test';
@@ -12,6 +13,12 @@ describe('regression', function() {
             uit.runs(function(window) {
                 expect(window.flag).toBe(true);
                 expect(window.location.hash).toBe('#test');
+            });
+        });
+        it('should return "#/page1.html"', function() {
+            uit.url( relPath + "#/page1.html");
+            uit.runs(function(window) {
+                expect(window.location.hash).toBe('#/page1.html');
             });
         });
     });
